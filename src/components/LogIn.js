98529b6b67a6363b9a66993a86c2bac1738f3f77
Luis Felipe <l_felipelizeu@hotmail.com>
2021-09-10 01:16:02 -0300

    Implemented the log in logic. When the user clicks on 'Entrar' the server will send a log in request while the inputs will be disabled and the button gets a animation, if the request fails, it will show the error message to the user and enable again the inputs and the button, if it successful the user will be redirected to '/hoje' route.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,16 +1,51 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import { Image, Container } from '../styles/InitialPage.js'
 import Logo from '../assets/images/logo.svg';
 import { Input } from '../styles/Input.js';
 import { Button } from '../styles/Button.js';
+import { logInTry } from '../service/trackit.js';
+import Loader from "react-loader-spinner";
 
 export default function LogIn () {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [disable, setDisable] = useState(false);
+    const history = useHistory();
+
+    function treatSuccess () {
+        history.push("/hoje");
+    }
+
+    function treatError (error) {
+        switch (error.response.status) {
+            case 401:
+                alert("Usuário e/ou senha inválidos!");
+                break;
+            default:
+                alert("Preencha os campos corretamente!");
+        }
+
+        setDisable(false);
+    }
+
+    function logIn () {
+        const user = {
+            email,
+            password
+        }
+
+        setDisable(true);
+
+        logInTry(user, treatSuccess, treatError);
+    }
+
     return (
         <Container>
             <Image src={Logo} />
-            <Input type="email" placeholder="email" />
-            <Input type="password" placeholder="senha" />
-            <Button>Entrar</Button>
+            <Input disabled={disable} type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <Input disabled={disable} type="password" placeholder="senha" alue={password} onChange={(e) => setPassword(e.target.value)} />
+            <Button onClick={logIn} disabled={disable}>{disable ? <Loader type="ThreeDots" height={45} color="#ffffff" />:"Entrar"}</Button>
             <Link to='/cadastro'>
                 Não tem uma conta? Cadastre-se!
             </Link>
--- src/service/trackit.js
+++ src/service/trackit.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/"
+const BASE_URL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit";
+
+function logInTry (user, treatSuccess, treatError) {
+    axios.post(`${BASE_URL}/auth/login`, user)
+        .then(treatSuccess)
+        .catch(treatError);
+}
 
 function sendSignUpToServer (body, treatSuccess, treatError) {
     axios.post(`${BASE_URL}/auth/sign-up`, body)
@@ -9,5 +15,6 @@ function sendSignUpToServer (body, treatSuccess, treatError) {
 }
 
 export {
+    logInTry,
     sendSignUpToServer,
 }
