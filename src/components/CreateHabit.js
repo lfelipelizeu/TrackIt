d589a33ebf19abe0ba905eca0ca25fb57d5de8c9
Luis Felipe <l_felipelizeu@hotmail.com>
2021-09-09 23:22:57 -0300

    Created the 'CreateHabit' component, the user can select and deselect the weekdays. Done some other adjustments

---
+++ src/components/CreateHabit.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { Button } from '../styles/Button.js';
+import { Input } from '../styles/Input.js';
+
+export default function CreateHabit () {
+    const weekdays = ["D","S","T","Q","Q","S","S"];
+
+    return (
+        <Box>
+            <Input type="text" placeholder="nome do hábito" />
+            <Weekdays>
+                {weekdays.map((weekday, index) => <Weekday key={index} day={weekday} />)}
+            </Weekdays>
+            <Buttons />
+        </Box>
+    );
+}
+
+function Weekday ({ day }) {
+    const [selected, setSelected] = useState(false);
+
+    function selectDay () {
+        selected ? setSelected(false):setSelected(true);
+    }
+
+    return (
+        <Day selected={selected} onClick={() => selectDay()}>{day}</Day>
+    );
+}
+
+function Buttons () {
+    return (
+        <ButtonsBox>
+            <CancelButton>Cancelar</CancelButton>
+            <Button>Salvar</Button>
+        </ButtonsBox>
+    );
+}
+
+const Box = styled.div`
+    width: 100%;
+    background-color: #ffffff;
+    padding: 15px;
+    margin-bottom: 30px;
+`;
+
+const Weekdays = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`;
+
+const Day = styled.div`
+    width: 30px;
+    height: 30px;
+    border: 1px solid #d4d4d4;
+    border-radius: 5px;
+    background-color: ${({ selected }) => selected ? '#cfcfcf':'transparent'};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: ${({ selected }) => selected ? '#ffffff':'#dbdbdb'};
+    margin-right: 5px;
+`;
+
+const ButtonsBox = styled.div`
+    width: 100%;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    button {
+        height: 100% !important;
+        font-size: 16px;
+        padding: 0 15px;
+    }
+`;
+
+const CancelButton = styled.button`
+    background-color: transparent;
+    border: none;
+    color: #52b6ff;
+`;
--- src/components/Habits.js
+++ src/components/Habits.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 import { Button } from '../styles/Button.js';
 import { PageTitle, Warning } from '../styles/MainPageStyles.js';
+import CreateHabit from './CreateHabit.js';
 
 export default function Habits () {
     return (
         <>
             <Title />
+            <CreateHabit />
             <Warning>
                 Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
             </Warning>
